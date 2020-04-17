6c9511f3ab0bb97f87f89920916c93fc61bc1516
Chagrdi <charlotte.grandjean@yahoo.fr>
2020-04-17 14:50:41 +0200

    accueil-afaireapressass

--- index.php
+++ index.php
@@ -8,12 +8,37 @@ if(!isset($_SESSION["username"])){
 require_once(dirname(__FILE__) . "/components/navbar.php");
 ?>
 
-<div class="container mb-5">
-    <h1>Liste des annonces</h1>
-    <div class="row">  
-    </div>
-</div>
+<!-- Petit encart de bienvenu -->
+<div class="presentation">
+<center>
+<h2>Bienvenue !</h2>
+<h4>Ici on aide ceux qui ont du mal</h4>
+<button type="button" class="btn btn-light">Voir les propositions</button>
+<center>
 
+</div>
+<div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-interval="10000" style="width: 100%; height: 90vh;">
+      <img src="https://images.unsplash.com/photo-1584265549884-cb8ea486a613?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-interval="2000" style="width: 100%; height: 90vh;">
+      <img src="https://images.unsplash.com/photo-1585435557343-3b092031a831?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" style="width: 100%; height: 90vh;">
+      <img src="https://images.unsplash.com/photo-1583947215259-38e31be8751f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!-- Fin de l'encart de bienvenue-->
 <?php
 require_once( dirname(__FILE__) . "/components/footer.php");
 ?>
